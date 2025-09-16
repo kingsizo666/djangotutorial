dc62ccfe2d32586af12fe9697b4aaac79e6ce6cf
Hristo Andonov <kingsizo666@gmail.com>
2025-09-17 01:32:44 +0200

    Initial commit with default version, tbd: Replace code with Django shortcuts

---
+++ mysite/polls/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    question_topic = models.CharField(max_length=30)
+    pub_date = models.DateTimeField("date publised")
+
+    # Whenever we query the question
+    # it will be represented by what the question is
+    # instead of its id
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
