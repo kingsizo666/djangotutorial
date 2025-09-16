bdf1267ad85be52e9c0ea4cd61b5375802d58023
Hristo Andonov <kingsizo666@gmail.com>
2025-09-17 01:51:38 +0200

    Replaced custom views with generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,31 +5,22 @@ from django.urls import reverse
 from .models import Choice, Question
 from django.template import loader
 from django.db.models import F
+from django.views import generic
 
-# upon request, this view will be returned
-#in order to access it, we need to map it to URL
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    # render is the shortcut for HTTPResponse
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-# a better way to write the exception
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
