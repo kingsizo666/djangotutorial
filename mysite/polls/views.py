285c5c49dc9cdb5f7814bbd137aa4ae03cfe0cf0
Hristo Andonov <kingsizo666@gmail.com>
2025-09-22 15:02:54 +0200

    Added tests for views and fixed code based on test outcomes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from .models import Choice, Question
 from django.template import loader
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """first filter only those that are not in the future,
+        then order in reversed pub_date and display 5"""
+        filtered_questions = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return filtered_questions
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
