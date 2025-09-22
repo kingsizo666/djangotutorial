ed9ad426d645bfa0e305c81b0a1fdcd30f836202
Hristo Andonov <kingsizo666@gmail.com>
2025-09-22 15:26:13 +0200

    Created custom admin view for managin Questions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
-admin.site.register(Question)
+
+# Default way of registering a model for admin
+# admin.site.register(Question)
+
+# Custom way to register a model for admin
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text", "question_topic"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
